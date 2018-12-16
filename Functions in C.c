1563e2a97dec37d3a6fad366b4b1a88c237ffc42
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2018-12-16 16:05:38 +0530

    big in 4number
    find greater separately for a,b and c,d
    finally, compare those two values

---
+++ Functions in C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int max_of_four(int a, int b, int c, int d);
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d)
+{
+    return (m1=a>b?a:b)>(m2=c>d?c:d)?m1:m2;
+}
